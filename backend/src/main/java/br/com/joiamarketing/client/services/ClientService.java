c6f6a76ece4f5629f04a74b0b18dc8f1ad3d93fd
MarceloJoia <sitejoia@hotmail.com>
2021-11-22 16:55:39 -0300

    Database Seeding

--- backend/src/main/java/br/com/joiamarketing/client/services/ClientService.java
+++ backend/src/main/java/br/com/joiamarketing/client/services/ClientService.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import br.com.joiamarketing.client.entities.Client;
 import br.com.joiamarketing.client.repositories.ClientRepository;
@@ -14,6 +15,7 @@ public class ClientService {
 	@Autowired
 	public ClientRepository repository;
 	
+	@Transactional(readOnly=true)
 	public List<Client> findAll(){
 		return repository.findAll();
 	}
