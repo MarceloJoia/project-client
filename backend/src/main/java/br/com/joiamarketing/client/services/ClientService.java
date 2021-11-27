220992aceb11350f6b2b300d30b5be977c0c55a3
MarceloJoia <sitejoia@hotmail.com>
2021-11-27 17:00:37 -0300

    Client DT

--- backend/src/main/java/br/com/joiamarketing/client/services/ClientService.java
+++ backend/src/main/java/br/com/joiamarketing/client/services/ClientService.java
@@ -1,11 +1,13 @@
 package br.com.joiamarketing.client.services;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import br.com.joiamarketing.client.dto.ClientDTO;
 import br.com.joiamarketing.client.entities.Client;
 import br.com.joiamarketing.client.repositories.ClientRepository;
 
@@ -16,7 +18,8 @@ public class ClientService {
 	public ClientRepository repository;
 	
 	@Transactional(readOnly=true)
-	public List<Client> findAll(){
-		return repository.findAll();
+	public List<ClientDTO> findAll(){
+		List<Client> list = repository.findAll();	
+		return list.stream().map(x -> new ClientDTO(x)).collect(Collectors.toList());
 	}
 }
