f797c1af3617faa0a23c5b93c4a388174e02261f
MarceloJoia <sitejoia@hotmail.com>
2021-12-04 12:40:57 -0300

    Client search

--- backend/src/main/java/br/com/joiamarketing/client/resources/ClientResource.java
+++ backend/src/main/java/br/com/joiamarketing/client/resources/ClientResource.java
@@ -1,9 +1,11 @@
 package br.com.joiamarketing.client.resources;
 
 import java.net.URI;
-import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort.Direction;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -12,6 +14,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
@@ -26,8 +29,14 @@ public class ClientResource {
 	public ClientService service;
 
 	@GetMapping
-	public ResponseEntity<List<ClientDTO>> findAll() {
-		List<ClientDTO> list = service.findAll();
+	public ResponseEntity<Page<ClientDTO>> findAllPage(
+			@RequestParam(value = "page", defaultValue = "0") Integer page,
+			@RequestParam(value = "linesPerPage", defaultValue = "12") Integer linesPerPage,
+			@RequestParam(value = "direction", defaultValue = "DESC") String direction,
+			@RequestParam(value = "orderBy", defaultValue = "moment") String orderBy
+			) {
+		PageRequest pegeRequest = PageRequest.of(page, linesPerPage, Direction.valueOf(direction), orderBy);
+		Page<ClientDTO> list = service.findAllPaged(pegeRequest);
 		return ResponseEntity.ok().body(list);
 	}
 
@@ -49,7 +58,7 @@ public class ClientResource {
 		dto = service.update(id, dto);
 		return ResponseEntity.ok().body(dto);
 	}
-	
+
 	@DeleteMapping(value = "/{id}")
 	public ResponseEntity<Void> delete(@PathVariable Long id) {
 		service.delete(id);
